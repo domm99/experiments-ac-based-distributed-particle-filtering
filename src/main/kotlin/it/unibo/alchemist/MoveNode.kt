1e1c3f37ad63488cc1f7bfe88d6181bfd16ad3db
davidedomini <davide.domini@unibo.it>
2025-11-27 14:11:34 +0000

    feat: add boundaries

--- src/main/kotlin/it/unibo/alchemist/MoveNode.kt
+++ src/main/kotlin/it/unibo/alchemist/MoveNode.kt
@@ -9,48 +9,68 @@ import it.unibo.alchemist.model.Environment
 import it.unibo.alchemist.model.actions.AbstractMoveNode
 import it.unibo.alchemist.model.positions.Euclidean2DPosition
 
-data class AgentState(
-    val x: Double,
-    val y: Double,
-    val vx: Double,
-    val vy: Double
-)
-
 class MoveNode<T> (
     environment: Environment<T, Euclidean2DPosition>,
     node: Node<T>,
     val xVel: Double,
     val yVel: Double,
+    val sideLength: Double
 ) : AbstractMoveNode<T, Euclidean2DPosition>(environment, node, true) {
 
     private val random = Random()
     private val sigmaSquared = 0.00035
     private val sigma = sqrt(sigmaSquared)
-    private var newVx = xVel
-    private var newVy = yVel
+    private var currentVx = xVel
+    private var currentVy = yVel
 
     override fun getNextPosition(): Euclidean2DPosition? {
         val currentPosition = environment.getPosition(node)
-        return computeNextPosition(AgentState(currentPosition.x, currentPosition.y, newVx, newVy))
+        var newPosition = computeNextPosition(currentPosition)
+        newPosition = checkBoundaries(newPosition)
+        return newPosition
     }
 
     override fun cloneAction(
         p0: Node<T?>?,
         p1: Reaction<T?>?
     ): Action<T?>? {
-        return MoveNode(environment, node, xVel, yVel)
+        return MoveNode(environment, node, xVel, yVel, sideLength)
     }
 
-    private fun computeNextPosition(currentState: AgentState): Euclidean2DPosition {
+    private fun computeNextPosition(currentPosition: Euclidean2DPosition): Euclidean2DPosition {
 
         val ux = random.nextGaussian() * sigma
         val uy = random.nextGaussian() * sigma
 
-        val newX = currentState.x + currentState.vx + (0.5 * ux)
-        val newY = currentState.y + currentState.vy + (0.5 * uy)
-        newVx = currentState.vx + ux
-        newVy = currentState.vy + uy
+        val newX = currentPosition.x + currentVx + (0.5 * ux)
+        val newY = currentPosition.y + currentVy + (0.5 * uy)
+        currentVx = currentVx + ux
+        currentVy = currentVy + uy
 
+        println("Current vx = $currentVx currentVy = $currentVy")
+
+        return Euclidean2DPosition(newX, newY)
+    }
+
+    private fun checkBoundaries(position: Euclidean2DPosition): Euclidean2DPosition {
+        var newX = position.x
+        var newY = position.y
+
+        if (newX < 0) {
+            newX = 0.0
+            currentVx = -currentVx
+        } else if (newX > sideLength) {
+            newX = sideLength
+            currentVx = -currentVx
+        }
+
+        if (newY < 0) {
+            newY = 0.0
+            currentVy = -currentVy
+        } else if (newY > sideLength) {
+            newY = sideLength
+            currentVy = -currentVy
+        }
         return Euclidean2DPosition(newX, newY)
     }
 
