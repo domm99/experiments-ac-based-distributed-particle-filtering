3a7e1c741bee5de9d5aaa4877c91a4b005c40003
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2025-12-23 16:45:39 +0100

    chore(build): add context parameters

--- build.gradle.kts
+++ build.gradle.kts
@@ -126,5 +126,6 @@ File(rootProject.rootDir.path + "/src/main/yaml")
 tasks.withType<KotlinCompilationTask<*>>().all {
     compilerOptions {
         allWarningsAsErrors = false
+        freeCompilerArgs.add("-Xcontext-parameters")
     }
 }
