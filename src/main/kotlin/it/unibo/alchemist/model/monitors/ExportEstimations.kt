0580a0f9d67bf013305ed2dfe7506f2032a182f3
Davide Domini <davide.domini@unibo.it>
2026-01-09 16:56:27 +0100

    feat: export also all the particles for each time step

--- src/main/kotlin/it/unibo/alchemist/model/monitors/ExportEstimations.kt
+++ src/main/kotlin/it/unibo/alchemist/model/monitors/ExportEstimations.kt
@@ -6,8 +6,11 @@ import it.unibo.alchemist.model.Time
 import it.unibo.alchemist.model.molecules.SimpleMolecule
 import it.unibo.alchemist.model.positions.Euclidean2DPosition
 import it.unibo.filtering.Point
+import it.unibo.filtering.Particle
 import java.io.File
 import java.util.Locale
+
+class Line(vararg val values: Any)
 
 /**
  * Exports the estimations made by filter nodes to CSV files upon simulation completion.
@@ -20,7 +23,36 @@ class ExportEstimations<T> : OutputMonitor<T, Euclidean2DPosition> {
         filters.forEach { filter ->
             val estimations = filter.getConcentration(SimpleMolecule("Estimations")) as MutableList<Point>
             val id = filter.id
-            exportToCsv("data/estimations_node-$id.csv", estimations)
+            exportToCsv(
+                "data/estimations_node-$id.csv",
+                "estimatedX,estimatedY",
+                "%.4f,%.4f",
+                estimations.map { Line(it.x, it.y) }
+            )
+
+            try {
+                val numberOfParticles = filter.getConcentration(SimpleMolecule("NumberOfParticles")) as Int
+                val particles = filter.getConcentration(SimpleMolecule("Particles")) as MutableList<MutableList<Particle>>
+                val header = (0 until numberOfParticles).joinToString(",") { "p_$it-X,p_$it-Y,p_$it-vX,p_$it-vY,p_$it-W" }
+                println("HEADER ${header.split(",").size}")
+                val format = (0 until numberOfParticles).joinToString(",") { "%.4f,%.4f,%.4f,%.4f,%.4f" }
+                val hist = particles.map { particleList ->
+                    val coordinates = particleList.flatMap { listOf(it.x, it.y, it.vx, it.vy, it.weight) }.toTypedArray()
+                    Line(*coordinates)
+                }
+                println("------------------------------------------------------------------------------------")
+                println(hist[0].values.size)
+                println("------------------------------------------------------------------------------------")
+                exportToCsv(
+                    "data/particles_node-$id.csv",
+                    header,
+                    format,
+                    hist
+                )
+            }catch (e: Exception) {
+                print(e)
+            }
+
         }
     }
 
@@ -29,19 +61,19 @@ class ExportEstimations<T> : OutputMonitor<T, Euclidean2DPosition> {
      * @param filename the name of the file to export to
      * @param history the list of points representing the history of estimations
      */
-    fun exportToCsv(filename: String, history: List<Point>) {
+    fun exportToCsv(filename: String, header: String, format: String, history: List<Line>) {
         File(filename).printWriter().use { out ->
             // Header
-            out.println("estimatedX,estimatedY")
+            out.println(header)
 
+            println("FORMAT ${format.split(",").size}")
             // Data
             history.forEach { step ->
-                println("${step.x},${step.y}")
+                println("STEP ${step.values.size}")
                 val line = String.format(
                     Locale.US,
-                    "%.4f,%.4f",
-                    step.x,
-                    step.y,
+                    format,
+                    *step.values
                 )
                 out.println(line)
             }
