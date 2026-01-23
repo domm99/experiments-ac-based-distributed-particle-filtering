b54eb2cd3371abcf096db68cb0a77f97f7dfdf85
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2026-01-23 17:41:26 +0100

    feat: refactor local filtering to incorporate leader-based particle convergence

--- src/main/kotlin/it/unibo/collektive/AggregateInformationAgentLeaderBased.kt
+++ src/main/kotlin/it/unibo/collektive/AggregateInformationAgentLeaderBased.kt
@@ -4,22 +4,16 @@ import it.unibo.alchemist.collektive.device.CollektiveDevice
 import it.unibo.alchemist.model.Environment
 import it.unibo.alchemist.model.Position
 import it.unibo.alchemist.model.molecules.SimpleMolecule
-import it.unibo.collektive.aggregate.Field
 import it.unibo.collektive.aggregate.api.Aggregate
-import it.unibo.collektive.aggregate.api.neighboring
 import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
 import it.unibo.collektive.alchemist.device.sensors.LocationSensor
+import it.unibo.collektive.stdlib.accumulation.convergeCast
 import it.unibo.collektive.stdlib.consensus.boundedElection
-import it.unibo.collektive.stdlib.consensus.globalElection
 import it.unibo.filtering.ParticleFilter
-import it.unibo.filtering.ParticleHistory
 import it.unibo.filtering.ParticleMemory
 import it.unibo.filtering.Point
-import it.unibo.filtering.div
 import it.unibo.filtering.distanceTo
-import it.unibo.filtering.plus
 import kotlin.math.exp
-import org.apache.commons.math3.random.RandomGenerator
 
 /**
  * The entrypoint of the simulation performing local information filtering.
@@ -28,60 +22,46 @@ fun Aggregate<Int>.informationFilterEntrypointLeaderBased(
     collektiveDevice: CollektiveDevice<*>,
     env: EnvironmentVariables,
     position: LocationSensor,
-) = context(env, collektiveDevice.randomGenerator, position) {
+) = context(env, collektiveDevice.randomGenerator, position, collektiveDevice) {
     val sideLength = env["SideLength"] as Int
-    val dist = collektiveDevice.environment.distanceFromNetworkCentroid(position.coordinates())
-    val weight = centralityWeight(dist, sideLength / 2.0) // the highest, the closest to the center
-    env["weight"] = weight to localId
-    val isLeader = boundedElection(strength = weight, bound = sideLength) == localId
-    env["isLeader"] = isLeader
     val numberOfParticles = env["NumberOfParticles"] as Int
+    val maxInitialSpeed = env["MaxInitialSpeed"] as Double
+    val isLeader = isLeaderBasedOnLocation(sideLength).also { env["isLeader"] = it }
+    env["sensing?"] = position.targetsPosition().firstOrNull()
     evolve(listOf<Point>()) { estimationsHistory ->
-        localFilterings(estimationsHistory, numberOfParticles, env["MaxInitialSpeed"], sideLength.toDouble())
+        sensorsExecution(isLeader, estimationsHistory, numberOfParticles, maxInitialSpeed, sideLength.toDouble())
     }.also { history ->
         env["Estimations"] = history
     }
 }
 
-/**
- * Performs local filtering using a Particle Filter to estimate the position
- * of a target based on neighborhood information.
- *
- * @param random the random generator for stochastic processes
- * @param position the location sensor providing target position and neighborhood data
- */
-context(random: RandomGenerator, position: LocationSensor, env: EnvironmentVariables)
-fun Aggregate<*>.localFilterings(
+context(device: CollektiveDevice<*>, position: LocationSensor)
+fun Aggregate<Int>.sensorsExecution(
+    isLeader: Boolean,
     estimationsHistory: List<Point>,
     numberOfParticles: Int,
     maxInitialSpeed: Double,
     sideLength: Double,
-): List<Point> = evolving(ParticleMemory(ParticleFilter(numberOfParticles, maxInitialSpeed, sideLength, random))) { memory ->
-    memory.particlesHistory.add(ParticleHistory(memory.filter.getAll()))
-    env["Particles"] = memory.particlesHistory
+): List<Point> = evolving(ParticleMemory(ParticleFilter(numberOfParticles, maxInitialSpeed, sideLength, device.randomGenerator))) { memory ->
+    val avgPoint = averageNeighborhoodPoint()
     val sampledParticles = memory.filter.resample()
-    val newParticles = memory.filter.predictParticles(sampledParticles)
-    memory.filter.updateWeights(newParticles, aaaa())
-    val estimation = memory.filter.estimatePosition()
-    val history = estimationsHistory + estimation
-    memory.yielding { history }
+    val predictedParticles = memory.filter.predictParticles(sampledParticles)//.take(max(1, ceil(numberOfParticles * 0.5).toInt())) // top 50%
+    val convergedParticles = convergeCast(predictedParticles, isLeader) { list1, list2 -> list1 + list2 }
+    val estimate = if (isLeader) {
+        val limitParticles = convergedParticles.sortedByDescending { it.weight }.take(numberOfParticles)
+        memory.filter.updateWeights(limitParticles, avgPoint)
+        memory.filter.estimatePosition()
+    } else {
+        avgPoint
+    }
+    memory.yielding { if (estimate != null ) estimationsHistory + estimate else estimationsHistory}
 }
 
-/**
- * Calculates the average position of the neighborhood perception of the target position, given by the location sensor.
- *
- * @param position the location sensor that provides the target position and its neighborhood information
- * @return the average position of the neighboring points
- */
-context(position: LocationSensor)
-fun Aggregate<*>.aaaa(): Point? {
-    val targetPosition: Point? = position.targetsPosition().firstOrNull()
-    val neighborsTargetPosition: Field<*, Point?> = neighboring(targetPosition)
-    val neighborsNonNullPoint = neighborsTargetPosition.all.list.mapNotNull { it.value }
-    return when {
-        neighborsNonNullPoint.isEmpty() -> null
-        else -> neighborsNonNullPoint.reduce(Point::plus) / neighborsNonNullPoint.size.toDouble()
-    }
+context(device: CollektiveDevice<*>, position: LocationSensor)
+fun Aggregate<Int>.isLeaderBasedOnLocation(bound: Int): Boolean  {
+    val dist = device.environment.distanceFromNetworkCentroid(position.coordinates())
+    val weight = centralityWeight(dist, bound / 2.0) // the highest, the closest to the center
+    return boundedElection(strength = weight, bound = bound) == localId
 }
 
 fun <T, P: Position<P>> Environment<T, P>.distanceFromNetworkCentroid(position: Point): Double {
