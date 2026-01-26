abd05f57670ff9e893810041c4115fe74f9d4e50
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2026-01-26 09:08:33 +0100

    feat: enhance sensorsExecution to utilize environment variables and improve particle estimation

--- src/main/kotlin/it/unibo/collektive/AggregateInformationAgentLeaderBased.kt
+++ src/main/kotlin/it/unibo/collektive/AggregateInformationAgentLeaderBased.kt
@@ -27,44 +27,52 @@ fun Aggregate<Int>.informationFilterEntrypointLeaderBased(
     val numberOfParticles = env["NumberOfParticles"] as Int
     val maxInitialSpeed = env["MaxInitialSpeed"] as Double
     val isLeader = isLeaderBasedOnLocation(sideLength).also { env["isLeader"] = it }
-    env["sensing?"] = position.targetsPosition().firstOrNull()
-    evolve(listOf<Point>()) { estimationsHistory ->
-        sensorsExecution(isLeader, estimationsHistory, numberOfParticles, maxInitialSpeed, sideLength.toDouble())
+    val estimations = env.getOrDefault("Estimations", listOf<Point>())
+    with(env) {
+        sensorsExecution(isLeader, estimations, numberOfParticles, maxInitialSpeed, sideLength.toDouble())
     }.also { history ->
         env["Estimations"] = history
     }
 }
 
-context(device: CollektiveDevice<*>, position: LocationSensor)
+context(device: CollektiveDevice<*>, position: LocationSensor, env: EnvironmentVariables)
 fun Aggregate<Int>.sensorsExecution(
     isLeader: Boolean,
     estimationsHistory: List<Point>,
     numberOfParticles: Int,
     maxInitialSpeed: Double,
     sideLength: Double,
-): List<Point> = evolving(ParticleMemory(ParticleFilter(numberOfParticles, maxInitialSpeed, sideLength, device.randomGenerator))) { memory ->
-    val avgPoint = averageNeighborhoodPoint()
-    val sampledParticles = memory.filter.resample()
-    val predictedParticles = memory.filter.predictParticles(sampledParticles)//.take(max(1, ceil(numberOfParticles * 0.5).toInt())) // top 50%
-    val convergedParticles = convergeCast(predictedParticles, isLeader) { list1, list2 -> list1 + list2 }
+): List<Point> = evolving(
+    ParticleMemory(
+        ParticleFilter(
+            numberOfParticles,
+            maxInitialSpeed,
+            sideLength,
+            device.randomGenerator,
+        ),
+    ),
+) { memory ->
+    val sensedPosition = position.targetsPosition().firstOrNull()
+    env["Particles"] = memory.filter.getAll()
+    val convergedMeasurements = convergeCast(listOfNotNull(sensedPosition), isLeader) { m1, m2 -> m1 + m2 }
     val estimate = if (isLeader) {
-        val limitParticles = convergedParticles.sortedByDescending { it.weight }.take(numberOfParticles)
-        memory.filter.updateWeights(limitParticles, avgPoint)
-        memory.filter.estimatePosition()
-    } else {
-        avgPoint
-    }
-    memory.yielding { if (estimate != null ) estimationsHistory + estimate else estimationsHistory}
+        memory.filter.predict()
+        memory.filter.updateWithMeasurements(convergedMeasurements)
+        val pos = memory.filter.estimatePosition()
+        memory.filter.resample()
+        pos
+    } else sensedPosition
+    memory.yielding { if (estimate != null) estimationsHistory + estimate else estimationsHistory  }
 }
 
 context(device: CollektiveDevice<*>, position: LocationSensor)
-fun Aggregate<Int>.isLeaderBasedOnLocation(bound: Int): Boolean  {
+fun Aggregate<Int>.isLeaderBasedOnLocation(bound: Int): Boolean {
     val dist = device.environment.distanceFromNetworkCentroid(position.coordinates())
     val weight = centralityWeight(dist, bound / 2.0) // the highest, the closest to the center
     return boundedElection(strength = weight, bound = bound) == localId
 }
 
-fun <T, P: Position<P>> Environment<T, P>.distanceFromNetworkCentroid(position: Point): Double {
+fun <T, P : Position<P>> Environment<T, P>.distanceFromNetworkCentroid(position: Point): Double {
     val filtersNode = this.nodes.filter { it.contains(SimpleMolecule("Filter")) }
     val sum = filtersNode.fold(0.0 to 0.0) { acc, next ->
         val nextNodePos = this.getPosition(next).coordinates // Add 10 to avoid negative positions .map { it + 10 }
@@ -75,7 +83,5 @@ fun <T, P: Position<P>> Environment<T, P>.distanceFromNetworkCentroid(position: 
     return center.distanceTo(position) // the smallest, the closest
 }
 
-fun centralityWeight(
-    distanceFromCentroid: Double,
-    sigma: Double,
-): Double = exp(-(distanceFromCentroid * distanceFromCentroid) / (2 * sigma * sigma))
+fun centralityWeight(distanceFromCentroid: Double, sigma: Double): Double =
+    exp(-(distanceFromCentroid * distanceFromCentroid) / (2 * sigma * sigma))
